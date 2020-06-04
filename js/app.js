8bd0458bc901060e2704f09ba134194499c91bec
Hexx King <hexxking13@gmail.com>
2020-06-04 08:33:38 -0700

    uncomment code blocks, fixed typos, started Q7 w/ arrays but does not work yet

--- js/app.js
+++ js/app.js
@@ -43,74 +43,75 @@ alert('Hey ' + yourName + '! It\'s great to meet you! Thanks for visting my site
 
 //--------Q1-----------country vs city----------------
 
-//var countryAnswer = prompt('Was Hexx raised in the city or the country?').toLowerCase();
-//if(countryAnswer === 'country') {
-//alert('OMG! Is it THAT obvious?!?!');
-//console.log('they answered \'Was Hexx raised in the city or the country\' correctly');
-//} else {
-//alert('Ha! Yes! No one has to know that I was actually raised in the swamps of Texas!');
-//console.log('they answered \'Was Hexx raised in the city or the country\' incorrectly');
-//}
+var countryAnswer = prompt('Was Hexx raised in the city or the country?').toLowerCase();
+if(countryAnswer === 'country') {
+  alert('OMG! Is it THAT obvious?!?!');
+  console.log('they answered \'Was Hexx raised in the city or the country\' correctly');
+} else {
+  alert('Ha! Yes! No one has to know that I was actually raised in the swamps of Texas!');
+  console.log('they answered \'Was Hexx raised in the city or the country\' incorrectly');
+}
 
 //--------Q2-----------motorcycles----------------
 
-//var motorcycleAnswer = prompt('Would you believe that Hexx loves to ride motorcycles?').toLowerCase();
-//if(motorcycleAnswer === 'yes') {
-//alert('Duh, who doesn\'t love a beautifully hand-painted cruiser? Varoom!');
-//console.log('they answered \'Would you believe that Hexx loves to ride motorcycles?\' correctly');
-//} else {
-//alert('Thats fair. I don\'t even have a car. But I do have my motorcycle license! ');
-//console.log('they answered \'Would you believe that Hexx loves to ride motorcycles?\' incorrectly');
-//}
+var motorcycleAnswer = prompt('Would you believe that Hexx loves to ride motorcycles?').toLowerCase();
+if(motorcycleAnswer === 'yes') {
+  alert('Duh, who doesn\'t love a beautifully hand-painted cruiser? Varoom!');
+  console.log('they answered \'Would you believe that Hexx loves to ride motorcycles?\' correctly');
+} else {
+  alert('Thats fair. I don\'t even have a car. But I do have my motorcycle license! ');
+  console.log('they answered \'Would you believe that Hexx loves to ride motorcycles?\' incorrectly');
+}
 
 //--------Q3-----------ballerina----------------
 
-//var ballerinaAnswer = prompt('Do you think Hexx has ever been a professional ballerina?').toLowerCase();
-//if(ballerinaAnswer === 'yes') {
-//alert('Yup. Ballet is the reason my pinky toes don\'t touch the ground.');
-//console.log('they answered \'Do you think Hexx has ever been a professional ballerina?\' correctly');
-//} else {
-//alert('Really? I mean, I know I don\'t come off as the most graceful but give me some credit! I was a ballerina for 18 years!');
-//console.log('they answered \'Do you think Hexx has ever been a professional ballerina?\' incorrectly');
-//}
+var ballerinaAnswer = prompt('Do you think Hexx has ever been a professional ballerina?').toLowerCase();
+if(ballerinaAnswer === 'yes') {
+  alert('Yup. Ballet is the reason my pinky toes don\'t touch the ground.');
+  console.log('they answered \'Do you think Hexx has ever been a professional ballerina?\' correctly');
+} else {
+  alert('Really? I mean, I know I don\'t come off as the most graceful but give me some credit! I was a ballerina for 18 years!');
+  console.log('they answered \'Do you think Hexx has ever been a professional ballerina?\' incorrectly');
+}
 
 //--------Q4-----------summer vs winter----------------
 
-//var summerAnswer = prompt('Does Hexx prefer summer or winter?').toLowerCase();
-//if(summerAnswer === 'summer') {
-//alert('Yes! Give me all the heat!');
-//console.log('they answered \'Does Hexx prefer summer or winter?\' correctly');
-//} else {
-//alert('Nooooo, snow is too cold to enjoy. Prove me wrong.');
-//console.log('they answered \'Does Hexx prefer summer or winter?\' incorrectly');
-//}
+var summerAnswer = prompt('Does Hexx prefer summer or winter?').toLowerCase();
+if(summerAnswer === 'summer') {
+  alert('Yes! Give me all the heat!');
+  console.log('they answered \'Does Hexx prefer summer or winter?\' correctly');
+} else {
+  alert('Nooooo, snow is too cold to enjoy. Prove me wrong.');
+  console.log('they answered \'Does Hexx prefer summer or winter?\' incorrectly');
+}
 
 //--------Q5-----------rollerskating--------------------
 
-//var rollerskatingAnswer = prompt('Does Hexx enjoy biking or rollerskating more?').toLowerCase();
-//if(rollerskatingAnswer === 'rollerskating') {
-//alert('Yeah! This is how we roll!');
-//console.log('they answered \'Does Hexx enjoy biking or rollerskating more?\' correctly');
-//} else {
-//alert('Naw, 8 wheels are better than 2! Later Sk8er!');
-//console.log('they answered \'Does Hexx enjoy biking or rollerskating more?\' incorrectly');
-//}
+var rollerskatingAnswer = prompt('Does Hexx enjoy biking or rollerskating more?').toLowerCase();
+if(rollerskatingAnswer === 'rollerskating') {
+  alert('Yeah! This is how we roll!');
+  console.log('they answered \'Does Hexx enjoy biking or rollerskating more?\' correctly');
+} else {
+  alert('Naw, 8 wheels are better than 2! Later Sk8er!');
+  console.log('they answered \'Does Hexx enjoy biking or rollerskating more?\' incorrectly');
+}
 
 //------Q6----------how many slices of pizza-----------
 
 // Shout out to Courtney for assisting with this code!
 //Code Review and Guidance by Aadi Gephart (roommate) and Eddie DelRio (mentor).
 
-// var attemptsCorrectAnswer = 0;
-// var slicesPrompt;
-// var guessNumberSlices;
-// var sliceAnswer = 'Hexx can eat 9 pizza slices in a single sitting!';
+var attemptsCorrectAnswer = 0;
+var slicesPrompt;
+var guessNumberSlices;
+var sliceAnswer = 'Hexx can eat 9 pizza slices in a single sitting!';
 
 while (attemptsCorrectAnswer < 4 && guessNumberSlices !== 9)
 {
-  slicesPrompt = prompt('How many slices of pizza fo you think Hexx can eat in one sitting?');
+  slicesPrompt = prompt('How many slices of pizza do you think Hexx can eat in one sitting?');
   guessNumberSlices = parseInt (slicesPrompt);
   //The parseInt() function parses a string argument and returns an integer of the specified radix (the base in mathematical numeral systems). https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+
   if (guessNumberSlices === 9) {
     alert('Well, congrats ' + yourName + '! Your\'e not wrong!' + sliceAnswer);
     break;
@@ -127,4 +128,13 @@ while (attemptsCorrectAnswer < 4 && guessNumberSlices !== 9)
   attemptsCorrectAnswer++;
 }
 
+//------Q7----------hair colors----------------
+// Q: What hair colors has Hexx rocked in the past?
 
+var hairColors = ['blonde', 'red', 'black', 'purple', 'pink', 'green', 'blue', 'silver'];
+var hairColorsAnswer = prompt('What hair colors has Hexx rocked in the past?');
+console.log(hairColorsAnswer);
+
+if (hairColorsAnswer === hairColors) {
+  alert('That\'s correct! ' + hairColorsAnswer + ' is ONE of em. Can you guess the rest?');
+}
